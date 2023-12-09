4d5099cc4040639c438da1e663240072ae2f432b
MicheleBellitti <michelebellitti272@gmail.com>
2023-12-09 14:27:18 +0100

    day 9 added

--- 2023/day09/solution.py
+++ 2023/day09/solution.py
@@ -6,30 +6,62 @@ Author: Michele
 import os
 from datetime import datetime
 from aocd import get_data, submit
+from utils.advent import *
+from itertools import pairwise
 
 # Solution here
-
+def extrapolate(sequence):
+        extrapolated = sequence[-1]
+        while True:
+            sequence = [j - i for i, j in pairwise(sequence)]
+            # print(sequence)
+            if sequence:
+                extrapolated += sequence[-1]
+            if all(n == 0 for n in sequence):
+                break
+        return extrapolated
+            
 def p1(inp):
-    pass
+    vals = 0
+    for seq in inp:
+        
+        
+        v = extrapolate(seq)
+        
+        vals += v
+        
+    return vals
 def p2(inp):
-    pass
+    
+    vals = 0
+    for seq in inp:
+        
+        
+        v = extrapolate(seq[::-1])
+        
+        vals += v
+        
+    return vals
     
 
 def process_input(year, day):
     inp = open(0, encoding='utf-8').read()
     # inp = get_data(day=day, year=year)
-    print(inp)
+    history = [list(map(int, line.split())) for line in inp.splitlines()]
+    # print(history)
     
     # code
-    p_1, p_2 = p1(inp), p2(inp)
-    submit(p_1, part='a', day=day, year=year)
+    p_1, p_2 = p1(history), p2(history)
+    submit(p_2, part='b', day=day, year=year)
     
     return p_1, p_2
     
 if __name__ == "__main__":
     
     y = datetime.now().year
-    d = int(input("Enter the day: "))
+    d = 9
+    
+    setup(y, d)
     
     # Process and output result
     print("Solution:", process_input(y, d))
