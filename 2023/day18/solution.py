0a0ad3e002529aad625f10a58dc543493aec0231
MicheleBellitti <michelebellitti272@gmail.com>
2023-12-20 00:11:05 +0100

    day 18, 19 added

--- 2023/day18/solution.py
+++ 2023/day18/solution.py
@@ -3,16 +3,73 @@ Advent of Code 2023
 Day 18
 Author: Michele
 """
-# Your solution here
+import os
+from datetime import datetime
+import pprint
+from aocd import get_data, submit
+from utils.advent import *
+from itertools import combinations
 
-def process_input():
-    inp = open(0, encoding='utf-8 ').read().splitlines()
+
+def p1(inp):
+    points = [(0, 0)]
+    dirs = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
+
+    b = 0
+
+    for line in inp:
+        d, n, _ = line
+        dr, dc = dirs[d]
+        n = int(n)
+        b += n
+        r, c = points[-1]
+        points.append((r + dr * n, c + dc * n))
+
+    A = abs(sum(points[i][0] * (points[i - 1][1] - points[(i + 1) % len(points)][1]) for i in range(len(points)))) // 2
+    i = A - b // 2 + 1
+    return i+b
+
+def p2(inp):
+    points = [(0, 0)]
+    dirs = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
+
+    b = 0
+
+    for line in inp:
+        _, _, x = line
+        x = x[2:-1]
+        dr, dc = dirs["RDLU"[int(x[-1])]]
+        n = int(x[:-1], 16)
+        b += n
+        r, c = points[-1]
+        points.append((r + dr * n, c + dc * n))
+
+    A = abs(sum(points[i][0] * (points[i - 1][1] - points[(i + 1) % len(points)][1]) for i in range(len(points)))) // 2
+    i = A - b // 2 + 1
+
+    return i+b
+    
+
+def process_input(year, day):
+    inp = open(0, encoding='utf-8').read().splitlines()
+    inp = [l.strip().split() for l in inp]
+    # inp = get_data(day=day, year=year)
+    # print(inp)
+    
     # code
-    pass
+    p_1, p_2 = p1(inp), p2(inp)
+    submit(p_1, part='a', day=day, year=year)
+    submit(p_2, part='b', day=day, year=year)
+    
+    return p_1, p_2
     
 if __name__ == "__main__":
     
-
+    y = datetime.now().year
+    d = 18
+    
+    setup(y, d)
+    
     # Process and output result
-    print("Solution:", process_input())
+    print("Solution:", process_input(y, d))
 
--- 2023/day19/solution.py
+++ 2023/day19/solution.py
@@ -3,16 +3,131 @@ Advent of Code 2023
 Day 19
 Author: Michele
 """
-# Your solution here
+import os
+from datetime import datetime
+from aocd import get_data, submit
+from utils.advent import *
 
-def process_input():
-    inp = open(0, encoding='utf-8 ').read().splitlines()
+# Solution here
+
+def p1(inp):
+    block1, block2 = inp.split("\n\n")
+
+    workflows = {}
+
+    for line in block1.splitlines():
+        name, rest = line[:-1].split("{")
+        rules = rest.split(",")
+        workflows[name] = ([], rules.pop())
+        for rule in rules:
+            comparison, target = rule.split(":")
+            key = comparison[0]
+            cmp = comparison[1]
+            n = int(comparison[2:])
+            workflows[name][0].append((key, cmp, n, target))
+
+    ops = {
+        ">": int.__gt__,
+        "<": int.__lt__
+    }
+
+    def accept(item, name = "in"):
+        if name == "R":
+            return False
+        if name == "A":
+            return True
+
+        rules, fallback = workflows[name]
+        
+        for key, cmp, n, target in rules:
+            if ops[cmp](item[key], n):
+                return accept(item, target)
+        
+        return accept(item, fallback)
+
+    total = 0
+
+    for line in block2.splitlines():
+        item = {}
+        for segment in line[1:-1].split(","):
+            ch, n = segment.split("=")
+            item[ch] = int(n)
+        if accept(item):
+            total += sum(item.values())
+    return total
+def p2(inp):
+    block1, _ = inp.split("\n\n")
+
+    workflows = {}
+
+    for line in block1.splitlines():
+        name, rest = line[:-1].split("{")
+        rules = rest.split(",")
+        workflows[name] = ([], rules.pop())
+        for rule in rules:
+            comparison, target = rule.split(":")
+            key = comparison[0]
+            cmp = comparison[1]
+            n = int(comparison[2:])
+            workflows[name][0].append((key, cmp, n, target))
+
+    def count(ranges, name = "in"):
+        if name == "R":
+            return 0
+        if name == "A":
+            product = 1
+            for lo, hi in ranges.values():
+                product *= hi - lo + 1
+            return product
+        
+        rules, fallback = workflows[name]
+
+        total = 0
+
+        for key, cmp, n, target in rules:
+            lo, hi = ranges[key]
+            if cmp == "<":
+                T = (lo, min(n - 1, hi))
+                F = (max(n, lo), hi)
+            else:
+                T = (max(n + 1, lo), hi)
+                F = (lo, min(n, hi))
+            if T[0] <= T[1]:
+                copy = dict(ranges)
+                copy[key] = T
+                total += count(copy, target)
+            if F[0] <= F[1]:
+                ranges = dict(ranges)
+                ranges[key] = F
+            else:
+                break
+        else:
+            total += count(ranges, fallback)
+                
+        return total
+
+    return count({key: (1, 4000) for key in "xmas"})
+    
+
+def process_input(year, day):
+    inp = open(0, encoding='utf-8').read()
+    # inp = get_data(day=day, year=year)
+    # print(inp)
+    
     # code
-    pass
+    p_1, p_2 = p1(inp), p2(inp)
+    submit(p_1, part='a', day=day, year=year)
+    submit(p_2, part='b', day=day, year=year)
+    
+    return p_1, p_2
     
 if __name__ == "__main__":
-   
-
+    
+    y = datetime.now().year
+    d = 19
+    
+    setup(y, d)
+    
     # Process and output result
-    print("Solution:", process_input())
+    print("Solution:", process_input(y, d))
 
